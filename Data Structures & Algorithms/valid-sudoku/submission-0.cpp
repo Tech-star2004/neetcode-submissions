b71ea420efecde6e1e3f17b25f5df9cb44fcc45f
Tech-star2004 <nandipatinarasimhareddy7@gmail.com>
2026-07-03 12:02:30 +0530

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        int row[9];
+        int col[9];
+        int box[9];
+        for(int r = 0; r < 9; r++){
+            for(int c = 0; c < 9; c++){
+                if(board[r][c] == '.') continue;
+                int val = board[r][c] - '1';
+                int mask = 1 << val;
+                int box_idx = (r / 3) * 3 + c / 3;
+                if((row[r] & mask) || (col[c] & mask) || (box[box_idx] & mask)) return false;
+                row[r] |= mask;
+                col[c] |= mask;
+                box[box_idx] |= mask;
+            }
+        }
+        return true;
+    }
+};
