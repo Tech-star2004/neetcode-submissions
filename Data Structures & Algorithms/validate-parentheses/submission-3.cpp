64a15da26cd76cd0f79933a06e1887426454638f
Tech-star2004 <nandipatinarasimhareddy7@gmail.com>
2026-07-04 13:49:44 +0530

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        if(s.length() % 2 != 0) return false;
+        stack<char> st;
+        for(int i = 0; i < s.length(); i++){
+            if(s[i] == '(' || s[i] == '{' || s[i] == '[') st.push(s[i]);
+            else{
+                if(st.empty()) return false;
+                if(st.top() == '(' && s[i] == ')') st.pop();
+                else if(st.top() == '{' && s[i] == '}') st.pop();
+                else if(st.top() == '[' && s[i] == ']') st.pop();
+                else return false;
+            }
+        }
+        return st.empty();
+    }
+};
