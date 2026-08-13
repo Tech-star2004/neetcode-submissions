13fffca5ed39456aabaf58c22a233adaa9200a65
Tech-star2004 <nandipatinarasimhareddy7@gmail.com>
2026-08-13 22:58:08 +0530

    Add: jump-game-ii - submission-1

---
+++ Data Structures & Algorithms/jump-game-ii/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        if(nums.size() == 1) return 0;
+        int jumps = 0;
+        int currjend = 0;
+        int fthst = 0;
+        for(int i = 0; i < nums.size(); i++){
+            fthst = max(fthst, i + nums[i]);
+            if(i == currjend){
+                jumps++;
+                currjend = fthst;
+            }
+            if(currjend >= nums.size() - 1) break;
+        }
+        return jumps;
+    }
+};
