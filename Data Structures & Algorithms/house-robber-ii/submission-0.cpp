dc2bafe78f597e1c5d1f9f3eee51add30036f1c2
Tech-star2004 <nandipatinarasimhareddy7@gmail.com>
2026-08-19 09:07:04 +0530

    Add: house-robber-ii - submission-0

---
+++ Data Structures & Algorithms/house-robber-ii/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int prev1 = 0;
+        int prev2 = 0;
+        for(int i = 0; i < nums.size() - 1; i++){
+            int currentsum = max(prev1, prev2 + nums[i]);
+            prev2 = prev1;
+            prev1 = currentsum;
+        }
+        int idx0n2 = prev1;
+        prev1 = 0;
+        prev2 = 0;
+        for(int i = 1; i < nums.size(); i++){
+            int currentsum = max(prev1, prev2 + nums[i]);
+            prev2 = prev1;
+            prev1 = currentsum;
+        }
+        int idx1n1 = prev1;
+        return max(idx0n2, idx1n1);
+    }
+};
